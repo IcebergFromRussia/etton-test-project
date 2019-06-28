7896411c3bd7b6a94f9cdec84454c326acd20af8
Albert <adalbert.iceberg@gmail.com>
2019-06-28 19:13:13 +0300

    front

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,5 +12,10 @@ require('../css/app.css');
 const $ = require('jquery');
 
 $('.addInOrderBtn').on( "click", function() {
-    console.log( $( this ) );
+    console.log( $( this )[0].getAttribute('productId') );
+    var selector = 'input[name='+$( this )[0].getAttribute('productId')+']';
+    $(selector)[0].value =
+        parseInt($(selector)[0].value)+1;
+    console.log($(selector));
+
 });
